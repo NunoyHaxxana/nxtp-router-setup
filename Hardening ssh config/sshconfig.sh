8b47e12d6ef36380f2cadec0d34805708f9608dd
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-06-12 13:52:30 +0700

    Update sshconfig.sh

--- Hardening ssh config/sshconfig.sh
+++ Hardening ssh config/sshconfig.sh
@@ -43,7 +43,7 @@ for PARAM in ${param[@]}
   
   
   echo -e "\e[1m\e[32m${param[1]} set to no\e[0m" && sleep 1
-  echo "${param[1]} no" >> ${file}
+  echo "${param[1]} yes" >> ${file}
 
   echo -e "\e[1m\e[32m${param[2]} set to yes\e[0m" && sleep 1
   echo "${param[2]} yes" >> ${file}
